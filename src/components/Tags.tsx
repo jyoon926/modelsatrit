c7a62c29276ed9eeaa45f89fb7ffa0c355e1dfc5
jyoon926 <jacobyoon926@gmail.com>
2024-09-06 12:48:54 -0400

    Hide tags on outside click

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { MdPerson } from 'react-icons/md';
 import { Sizes } from '../utils/Enums';
 import { Tag } from '../utils/Types';
@@ -10,10 +10,29 @@ interface Props {
 
 export default function Tags({ tags }: Props) {
   const [viewTags, setViewTags] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setViewTags(false);
+      }
+    };
+
+    if (viewTags) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [viewTags]);
+
   return (
     <>
       {tags && tags.length > 0 && (
-        <div className="flex flex-row items-center gap-1 p-2">
+        <div className="flex flex-row items-center gap-1 p-2" ref={ref}>
           <div
             className={`p-1 rounded-full shadow text-background duration-300 bg-foreground/60 hover:bg-foreground hover:opacity-100 cursor-pointer`}
             onClick={() => setViewTags((prev) => !prev)}
