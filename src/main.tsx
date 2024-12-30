5e50f1497defc03256c3536054ddf22271a14f0d
jyoon926 <jacobyoon926@gmail.com>
2024-12-30 12:32:38 -0500

    Import firebase config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import './FirebaseConfig.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
